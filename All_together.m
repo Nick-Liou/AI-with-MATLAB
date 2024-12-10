972d87c8a7034597a5bbe83ac63026ad9d695463
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2024-12-10 15:06:43 +0200

    Extra figure

--- All_together.m
+++ All_together.m
@@ -4,7 +4,11 @@ close all ;
 % Load data 
 
 % load('MobileSensorData//sensorlog_20241210_103638.mat');
-load('MobileSensorData//sensorlog_20241210_103145.mat');
+% load('MobileSensorData//sensorlog_20241210_103145.mat');
+
+
+load("Train_data\ActivityLogs.mat")
+Acceleration = unknownAcceleration;
 
 
 if exist("Position","var") == 1
