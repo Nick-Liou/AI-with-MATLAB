ae5be2b280d0d24f7b414a62352d55e22663dfe5
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2024-12-10 15:49:02 +0200

    box plots

--- moving_plots.m
+++ moving_plots.m
@@ -20,6 +20,15 @@ ylabel('Acceleration (m/s^2)')
 
 
 
+% Create the boxplot
+figure;
+boxplot(Total_movingStats);
+xlabel('Run');
+ylabel('Accelaration m/s^2');
+title('Comparison of Activities');
+
+
+
 
 
 % Process for Walk Acceleration
@@ -39,6 +48,14 @@ ylabel('Acceleration (m/s^2)')
 
 
 
+% Create the boxplot
+figure;
+boxplot(Total_movingStats);
+xlabel('Walk');
+ylabel('Accelaration m/s^2');
+title('Comparison of Activities');
+
+
 % Process for Walk Acceleration
 Acceleration = sitAcceleration;
 Total_movingStats = computeMovingStats(sqrt(Acceleration.X.^2 + Acceleration.Y.^2 + Acceleration.Z.^2));
@@ -51,6 +68,14 @@ plot(Acceleration.Timestamp, Total_movingStats)
 legend("Acceleration","movingAverage" ,"movingMedian" ,"movingStd" ,"movingMax" ,"movingMin" ,"movingMad" ,"Location","best")
 title("Sit Acceleration (Moving Statistics)")
 ylabel('Acceleration (m/s^2)')
+
+
+% Create the boxplot
+figure;
+boxplot(Total_movingStats);
+xlabel('Sit');
+ylabel('Accelaration m/s^2');
+title('Comparison of Activities');
 
 
 
