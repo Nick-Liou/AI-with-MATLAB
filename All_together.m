d29ed213b715ac187652e34b8c3051c4696f466c
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2024-12-10 16:16:16 +0200

    Delete extra file

--- All_together.m
+++ All_together.m
@@ -11,6 +11,9 @@ load('MobileSensorData\sensorlog_20241210_103145.mat');
 % load("Train_data\ActivityLogs.mat")
 % Acceleration = unknownAcceleration;
 
+% Extra test data provided
+% load("ExampleData.mat")
+
 
 if exist("Position","var") == 1
     if  size(Position,1) > 2
